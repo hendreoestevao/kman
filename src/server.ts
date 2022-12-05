0542c4fd251784f3cb2bc2434d01bb9268b893d7
hendreoestevao <hendreosantos1@outlook.com>
2022-12-05 13:43:22 -0300

    conectando ao banco

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,19 @@
 import express, {Request, Response } from 'express'
+import mongoose from 'mongoose'
 
 
 const app = express()
 
-app.get('./',(req: Request,res: Response) => {
+mongoose.connect('mongodb+srv://hendreo:hendreo@cluster0.gbqwmue.mongodb.net/kman?retryWrites=true&w=majority')
+.then(()=> (
+    console.log('Connect')
+))
+.catch(err => (console.log(err)))
+
+app.get('/',(req: Request,res: Response) => {
     return res.send('hellou word')
 })
 
 app.listen(3333)
+
+//MONGO_URL = mongodb+srv://hendreo:hendreo@cluster0.gbqwmue.mongodb.net/kman?retryWrites=true&w=majority
